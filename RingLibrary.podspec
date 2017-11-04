b023e15ae2527474f011730fc4293c5cc5943ca3
Marvin Martin Etoga Mouroum <m.mouroum@campus.tu-berlin.de>
2017-11-04 11:26:17 +0100

    initial commit

---
+++ RingLibrary.podspec
@@ -0,0 +1,28 @@
+
+Pod::Spec.new do |s|
+
+  s.name         = "RingLibrary"
+  s.version      = "1.0.0"
+  s.summary      = "A Progress Ring with a Button inside."
+
+
+  s.description  = "a stylish simple way to combine progress and button. 3 different color shemes available plus gradient option"
+
+  s.homepage     = "https://mouroumm.wixsite.com/marvinmouroum"
+
+  s.license      = "MIT"
+
+
+  s.author             = { "Marvin Martin Etoga Mouroum" => "mouroum.m@gmail.com" }
+
+   s.platform     = :ios, "11.0"
+
+
+  s.source       = { :git => "https://github.com/marvinmouroum/ProgressRing.git", :tag => "1.0.1" }
+   #s.source       = { :path => '.' }
+
+  s.source_files  = "RingLibrary", "RingLibrary/**/*.{h,m,swift}"
+
+   s.pod_target_xcconfig = { 'SWIFT_VERSION' => '4' }
+
+end
---
+++ RingLibraryTests/RingLibraryTests.swift
@@ -0,0 +1,36 @@
+//
+//  RingLibraryTests.swift
+//  RingLibraryTests
+//
+//  Created by MacBook Pro Accounting  on 04.11.17.
+//  Copyright © 2017 bantuApps. All rights reserved.
+//
+
+import XCTest
+@testable import RingLibrary
+
+class RingLibraryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
